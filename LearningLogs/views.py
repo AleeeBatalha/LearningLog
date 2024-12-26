8794d97355be98801f30bc2c5e892cefc4cbceef
Francisco Batalha <you@example.com>
2024-12-26 18:04:46 -0300

    up

--- LearningLogs/views.py
+++ LearningLogs/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    """Pagina principal do LearningLog"""
+    return render(request, 'LearningLogs/index.html')
