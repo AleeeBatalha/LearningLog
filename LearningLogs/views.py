13cb58973005c955725db56a5895afc327748cef
Francisco Batalha <you@example.com>
2024-12-27 09:41:52 -0300

    up

--- LearningLogs/views.py
+++ LearningLogs/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Topic
 
 def index(request):
     """Pagina principal do LearningLog"""
     return render(request, 'LearningLogs/index.html')
+
+def topics(request):
+    """Mostra a todos os assuntos"""
+    topic = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'LearningLogs/topics.html', context)
